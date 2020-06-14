42f4de8b032d116538e47a9b8f8d8757ccad0db9
Elliot <elliot.borryn@student.odisee.be>
2020-06-14 11:15:01 +0200

    Initial Create

---
+++ src/main/java/me/bluemoonredsun/myfirstspigotplugin/MyFirstSpigotPlugin.java
@@ -0,0 +1,17 @@
+package me.bluemoonredsun.myfirstspigotplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MyFirstSpigotPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
